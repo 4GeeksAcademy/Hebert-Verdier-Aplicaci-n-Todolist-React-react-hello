20b2a159e97ec52d2e1b275aac7caca3604c9ace
HSimonVS <hebert.verdier@estudiantes.utec.edu.uy>
2024-07-09 17:07:46 +0000

    first commit

--- src/js/component/list.jsx
+++ src/js/component/list.jsx
@@ -1,33 +1,57 @@
 import React from "react";
-import {useState} from "react";
+import { useState } from "react";
 
 const List = () => {
 	// declaracion de estado
 	const [inputTarea, setInputTarea] = useState('');
-    const [lista, setLista] = useState([]);
+	const [lista, setLista] = useState([]);
+	// const [mostrar, setMostrar] = useState('none');
+
 
 	function handleTarea(event) {
 		setInputTarea(event.target.value);
 	}
+
 	function agregarTarea(e) {
 		e.preventDefault()
-		setLista(inputTarea);
+		inputTarea.trim()
+		if (inputTarea != '') {
+			const nuevaTarea = {
+				id: lista.length + 1,
+				texto: inputTarea,
+			}
+			setLista([...lista, nuevaTarea]);
+			setInputTarea('');
+		}
 	}
-	function eliminarTarea() {
-	}
+	function eliminarTarea(id) {
+		const newLista = lista.filter(item => item.id !== id);
+		setLista(newLista);
+	};
+
+
+	// function mostrarIcono() {
+	// 	setMostrar('flex')
+	// }
 
 	return (
-		<div>
-			<input type="text" onChange={handleTarea} value={inputTarea}/>
-			<button className="btn" onClick={(e)=>agregarTarea(e)}>Agregar</button>
+		<div className="d-flex flex-column align-items-center">
+			<h1>todos</h1>
+			<div className="d-flex flex-row">
+				<input className='' type="text" onChange={handleTarea} value={inputTarea} />
+				<button className="btn btn-primary" onClick={agregarTarea}>Agregar</button>
+			</div>
 			<div>
-				<ul>
-					{lista.map((item)=>(
-						<li>{item}</li>
+				<ul className="float-start">
+					{lista.map((item, index) => (
+						<li key={index} className="item-li d-flex flex-row fs-4 float-end">
+							{item.texto}
+							<button className="btn btn-light" onClick={() => eliminarTarea(item.id)} style={{ fontSize: '10px' }}>X</button>
+						</li>
 					))}
 				</ul>
 			</div>
-		</div>
+		</div >
 	);
 };
 
